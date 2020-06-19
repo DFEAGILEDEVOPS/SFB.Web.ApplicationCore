2b3e98c38a279980a864826edf21344e7acb5092
Serkan Sozer <eserkansozer@hotmail.com>
2020-06-19 14:34:37 +0100

    AB#41360 - Size lookup indefinite values support

--- Entities/SADSizeLookupDataObject.cs
+++ Entities/SADSizeLookupDataObject.cs
@@ -6,7 +6,7 @@
         public string OverallPhase { get; set; }
         public bool? HasSixthForm { get; set; }
         public decimal NoPupilsMin { get; set; }
-        public decimal NoPupilsMax { get; set; }
+        public decimal? NoPupilsMax { get; set; }
         public string SizeType { get; set; }
     }
 }
