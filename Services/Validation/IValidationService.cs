52a90fd929a5dd0f42f7d3b98b73f7dd48d2bfbd
Serkan Sozer <eserkansozer@hotmail.com>
2020-12-01 14:53:22 +0000

    AB#41359

--- Services/Validation/IValidationService.cs
+++ Services/Validation/IValidationService.cs
@@ -5,7 +5,9 @@
         string ValidateNameParameter(string name);
         string ValidateTrustNameParameter(string name);
         string ValidateLocationParameter(string location);
+        string ValidateLocationParameterForComparison(string location);
         string ValidateLaCodeParameter(string laCode);
+        string ValidateLaCodeParameterForComparison(string laCode);
         string ValidateLaNameParameter(string laName);
         string ValidateLaCodeNameParameter(string laCodeName);
         string ValidateSchoolIdParameter(string schoolId);
