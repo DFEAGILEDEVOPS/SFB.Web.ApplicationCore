2fae77ca622fa9df38480625e6df4e13f293940f
Serkan Sozer <eserkansozer@hotmail.com>
2021-01-18 17:38:50 +0000

    AB#44796

--- Models/BenchmarkChartData.cs
+++ Models/BenchmarkChartData.cs
@@ -51,6 +51,6 @@ namespace SFB.Web.ApplicationCore.Models
         public string ProgressScoreType { get; set; }
         
         [JsonProperty(PropertyName = "topsen")]
-        public List<KeyValuePair<string, int>> TopSEN { get; set; }
+        public List<KeyValuePair<string, decimal>> TopSEN { get; set; }
     }
 }
