8c662f5f4295e200d5cb0093ee81acaa2dd47042
Serkan Sozer <eserkansozer@hotmail.com>
2021-06-10 15:04:47 +0100

    AB#46091

--- Models/SelfAssesmentModel.cs
+++ Models/SelfAssesmentModel.cs
@@ -14,7 +14,7 @@ namespace SFB.Web.ApplicationCore.Models
 
         public string LondonWeighting { get; private set; }
 
-        public decimal NumberOfPupilsLatestTerm { get; private set; }
+        public decimal? NumberOfPupilsLatestTerm { get; private set; }
 
         public string OfstedRating { get; private set; }
 
@@ -26,9 +26,9 @@ namespace SFB.Web.ApplicationCore.Models
 
         public string ProgressScoreType { get; private set; }
 
-        public decimal Progress8Banding { get; private set; }
+        public decimal? Progress8Banding { get; private set; }
         
-        public decimal FSMLatestTerm { get; private set; }
+        public decimal? FSMLatestTerm { get; private set; }
 
         public string LatestTerm { get; set; }
 
@@ -40,15 +40,15 @@ namespace SFB.Web.ApplicationCore.Models
 
         public string OverallPhase { get; set; }
 
-        public decimal TotalExpenditureLatestTerm { get; set; }
+        public decimal? TotalExpenditureLatestTerm { get; set; }
 
-        public decimal TotalIncomeLatestTerm { get; set; }
+        public decimal? TotalIncomeLatestTerm { get; set; }
 
-        public decimal TeachersTotalLastTerm { get; set; }
+        public decimal? TeachersTotalLastTerm { get; set; }
 
-        public decimal TeachersLeaderLastTerm { get; set; }
+        public decimal? TeachersLeaderLastTerm { get; set; }
 
-        public decimal WorkforceTotalLastTerm { get; set; }
+        public decimal? WorkforceTotalLastTerm { get; set; }
 
         public List<string> AvailableScenarioTerms { get; set; }
         public SADSizeLookupDataObject SadSizeLookup { get; set; }
@@ -60,21 +60,21 @@ namespace SFB.Web.ApplicationCore.Models
             string overallPhase, 
             string financeType,
             string londonWeighting, 
-            decimal numberOfPupils, 
-            decimal fsm,
+            decimal? numberOfPupils, 
+            decimal? fsm,
             string ofstedRating,
             DateTime? ofstedInspectionDate,
             decimal? p8Score,
             decimal? ks2Score,
             string progressScoreType,
-            decimal progress8Banding,
+            decimal? progress8Banding,
             bool hasSixthForm,
-            decimal totalExpenditure,
-            decimal totalIncome,
+            decimal? totalExpenditure,
+            decimal? totalIncome,
             string latestTerm,
-            decimal teachersTotal,
-            decimal teachersLeader,
-            decimal workforceTotal,
+            decimal? teachersTotal,
+            decimal? teachersLeader,
+            decimal? workforceTotal,
             bool isReturnsComplete,
             bool doReturnsExist)
         {
@@ -101,5 +101,17 @@ namespace SFB.Web.ApplicationCore.Models
             IsReturnsComplete = isReturnsComplete;
             DoReturnsExist = doReturnsExist;
         }
+
+        public SelfAssesmentModel(long urn,
+            string name,
+            string overallPhase,
+            string financeType,
+            string ofstedRating,
+            DateTime? ofstedInspectionDate,
+            string londonWeighting,
+            bool hasSixthForm) :
+                this(urn, name, overallPhase, financeType, londonWeighting, null, null, ofstedRating, ofstedInspectionDate,
+                null, null, null, null, hasSixthForm, null, null, null, null, null, null, false, false)
+         { }
     }
 }
