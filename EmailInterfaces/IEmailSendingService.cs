10925e40deb22f03d5c9738faea2f3f6a3893925
Serkan Sozer <serkan@groovytechs.co.uk>
2021-09-02 14:36:56 +0100

    AB#47851

--- EmailInterfaces/IEmailSendingService.cs
+++ EmailInterfaces/IEmailSendingService.cs
@@ -6,8 +6,10 @@ namespace SFB.Web.ApplicationCore.Services
 {
     public interface IEmailSendingService
     {
-        Task<string> SendUserEmailAsync(string toAddress, Dictionary<String, dynamic> placeholders);
-        Task<string> SendDfEEmailAsync(string toAddress, Dictionary<String, dynamic> placeholders);
+        Task<string> SendDataQueryUserEmailAsync(string toAddress, Dictionary<String, dynamic> placeholders);
+        Task<string> SendDataQueryDfEEmailAsync(string toAddress, Dictionary<String, dynamic> placeholders);        
+        Task<string> SendContactUsUserEmailAsync(string toAddress, Dictionary<String, dynamic> placeholders);
+        Task<string> SendContactUsDfEEmailAsync(string toAddress, Dictionary<String, dynamic> placeholders);
         Task<string> SendGetInvolvedEmailAsync(string toAddress, Dictionary<String, dynamic> placeholders);
     }
 }
