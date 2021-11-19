ce6fac8f87b6233f85357e5e50cc9860a8cbde44
Serkan Sozer <serkan@groovytechs.co.uk>
2021-11-19 17:28:15 +0000

    Tests

---
+++ SFB.Web.UnitTests/GeoCodeCalcTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using SFB.Web.ApplicationCore.Helpers;
+
+namespace SFB.Web.UnitTests
+{
+    public class GeoCodeCalcTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalcDistanceShouldReturnCorrectValue()
+        {
+            var dist = GeoCodeCalc.CalcDistance("36", "26", "42", "45");
+            Assert.AreEqual(1768933.4399999999d, dist);
+        }
+
+    }
+}
