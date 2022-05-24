d3ac2fdf7e58071c7b879055507915415341f40c
Jon Banner <jon@jonbanner.net>
2022-05-24 13:22:51 +0100

    53125 : revert interface

--- SFB.Artifacts.ApplicationCore/CachingInterfaces/IBicComparisonResultCachingService.cs
+++ SFB.Artifacts.ApplicationCore/CachingInterfaces/IBicComparisonResultCachingService.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using SFB.Web.ApplicationCore.Models;
+﻿using SFB.Web.ApplicationCore.Models;
 
 namespace SFB.Web.ApplicationCore.Services
 {
@@ -7,8 +6,5 @@ namespace SFB.Web.ApplicationCore.Services
     {
         ComparisonResult GetBicComparisonResultByUrn(long urn);
         void StoreBicComparisonResultByUrn(long urn, ComparisonResult comparisonResult);
-        
-        Task<bool> CscpHasPage(int urn, bool isMat);
-        Task<bool> GiasHasPage(int urn, bool isMat);
     }
 }
