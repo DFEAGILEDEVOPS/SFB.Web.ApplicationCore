807d779d25e4d3effbf8a4a7e5d416c88c1fdf6f
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-03-07 11:43:50 +0000

    fix: Added custom converter for deserializing expected `int?` values from `decimal?` in source Cosmos document

---
+++ SFB.Artifacts.ApplicationCore/Entities/Converters/Int32JsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SFB.Web.ApplicationCore.Entities.Converters
+{
+    public class Int32JsonConverter: JsonConverter<int?>
+    {
+        public override void WriteJson(JsonWriter writer, int? value, JsonSerializer serializer)
+        {
+            // default
+            serializer.Serialize(writer, value);
+        }
+
+        public override int? ReadJson(JsonReader reader, Type objectType, int? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+            
+            return Convert.ToInt32(reader.Value?.ToString());
+        }
+    }
+}
